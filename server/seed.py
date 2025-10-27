e297c58be810940de03e908abb2ce3c75df20db8
schnei000 <walkerstfleur007@gmail.com>
2025-10-26 19:08:03 -0700

    modif

--- server/seed.py
+++ server/seed.py
@@ -7,17 +7,17 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-    p1 = Professeur(nom="Professeur elice")
+    p1 = Professeur(nom="Professeur  alice")
     p2 = Professeur(nom="Professeur Bob")
     db.session.add_all([p1, p2])
     db.session.commit()
 
-    c1 = Cours(titre="Flask pour débutants", professeur_id=p1.id)
+    c1 = Cours(titre="Flask  pour  débutants", professeur_id=p1.id)
     c2 = Cours(titre="React avancé", professeur_id=p2.id)
     db.session.add_all([c1, c2])
     db.session.commit()
 
-    l1 = Lecon(titre="Introduction à Flask", contenu="CRUD, REST, SQLAlchemy", cours_id=c1.id)
+    l1 = Lecon(titre="Introduction  à  Flask", contenu="CRUD, REST, SQLAlchemy", cours_id=c1.id)
     l2 = Lecon(titre="React Router", contenu="Routes dynamiques", cours_id=c2.id)
     db.session.add_all([l1, l2])
     db.session.commit()
